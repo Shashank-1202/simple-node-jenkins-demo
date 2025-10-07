794577aab6d91577a603d16bb33f97950e3cac11
Shashank <shashankiyer007@gmail.com>
2025-10-08 02:37:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Hello from Jenkins-deployed app!'));
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on ${port}`));
+app.listen(port, '0.0.0.0', () => console.log(`Listening on ${port}`));
 
